38f1af51947ce709f141dd994fa52a6d6a4ba4e4
Sean O'Connor <sean.ocon@gmail.com>
2020-01-20 20:14:31 +0000

    Added new route to allbooks

--- run.py
+++ run.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+    
+@app.route("/allbooks")
+def all():
+    return render_template("allbooks.html")
 
 
 if __name__ =="__main__":
